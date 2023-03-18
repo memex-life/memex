f2e2f36aa1d713ef4416b538491edbb7564b866a
Hao Ke <i@kehao.me>
2023-03-18 14:43:00 -0400

    config openAI apikey

--- src/background.ts
+++ src/background.ts
@@ -8,32 +8,44 @@ interface DomainCounts {
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({ domainCounts: {} });
+  chrome.storage.sync.get('apiKey', ({ apiKey }) => {
+    if (!apiKey) {
+      chrome.runtime.openOptionsPage();
+    }
+  });
 });
 
+
+
+
+
 chrome.runtime.onMessage.addListener((message: Message, sender, sendResponse) => {
+  console.log("message received");
+  chrome.storage.sync.get("apiKey", (data) => {
+    const apiKey = data.apiKey;
+    if (!apiKey) {
+      chrome.runtime.openOptionsPage();
+    }
+    console.log("API KEY: " + apiKey);
+
+  });
+
+
   const { textContent, metadata } = message;
   console.log(`Received message from URL: ${metadata.url}, timestamp: ${metadata.timestamp}`);
-  console.log(`Content: ${textContent.slice(0, 100)}...`);
+  // console.log(`Content: ${textContent.slice(0, 100)}...`);
 
-  // Get the domain from the URL
   const url = new URL(metadata.url);
   const domain = url.hostname;
 
-  // Get the current domain counts from storage
   chrome.storage.local.get('domainCounts', (result) => {
     const domainCounts = result.domainCounts as DomainCounts || {};
     const count = domainCounts[domain] || 0;
 
-    // Increment the count for this domain
     domainCounts[domain] = count + 1;
-
-    // Save the updated domain counts to storage
     chrome.storage.local.set({ domainCounts });
 
-    // Get the total message count
     const totalCount = Object.values(domainCounts).reduce((acc, val) => acc + val, 0);
-
-    // Set the badge text to the total message count
     chrome.action.setBadgeText({ text: `${totalCount}` });
   });
 });
