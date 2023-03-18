e3fce4a1ffc721a00dfc98e0bc53afaad58a6cde
Hao Ke <i@kehao.me>
2023-03-18 13:44:59 -0400

    message bubbles

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,14 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, TextField, Typography, Button } from '@material-ui/core';
+import { styled } from "@mui/material/styles";
 
 interface Message {
   text: string;
   sender: 'user' | 'bot';
 }
 
+interface MessageBubbleProps {
+  sender: 'user' | 'bot';
+}
+
+const MessageBubble = styled(Box)<MessageBubbleProps>`
+  background-color: ${({ theme, sender }) =>
+    sender === 'user' ? theme.palette.primary.main : theme.palette.grey[300]};
+  color: ${({ theme, sender }) => (sender === 'user' ? theme.palette.primary.contrastText : theme.palette.text.primary)};
+  padding: 8px 12px;
+  border-radius: ${({ sender }) =>
+    sender === 'user' ? '16px 16px 0px 16px' : '16px 16px 16px 0px'};
+  margin: 4px 0;
+  max-width: 70%;
+  word-wrap: break-word;
+`;
+
 const ChatBox: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const greetingMessage: Message = {
+      text: 'Hello! How may I help you today?',
+      sender: 'bot',
+    };
+    setMessages([greetingMessage]);
+  }, []);
 
   const handleSend = () => {
     if (input.trim()) {
@@ -27,9 +52,14 @@ const ChatBox: React.FC = () => {
     <Box display="flex" flexDirection="column" alignItems="center" p={2} width="300px">
       <Box display="flex" flexDirection="column" width="100%" mb={2} maxHeight="300px" overflow="auto">
         {messages.map((message, index) => (
-          <Typography key={index} align={message.sender === 'user' ? 'right' : 'left'}>
-            {message.text}
-          </Typography>
+          <Box
+            key={index}
+            alignSelf={message.sender === 'user' ? 'flex-end' : 'flex-start'}
+          >
+            <MessageBubble sender={message.sender}>
+              <Typography variant="body1">{message.text}</Typography>
+            </MessageBubble>
+          </Box>
         ))}
       </Box>
       <Box display="flex" width="100%">
